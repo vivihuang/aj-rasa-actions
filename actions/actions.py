0f595afd58e8e6226b22f6bd553c5fb1194e9b69
Vivi🐳Ruiwei Huang <rwhuang@thoughtworks.com>
2019-11-12 13:47:49 +0800

    Add actions for finding course.

--- actions/actions.py
+++ actions/actions.py
@@ -1,3 +1,4 @@
+from random import random
 from rasa_sdk import Action
 
 
@@ -19,4 +20,22 @@ class ActionChitchat(Action):
             "ask_time",
         ]:
             dispatcher.utter_template("utter_" + intent, tracker)
+        return []
+
+
+class ActionSearchCourse(Action):
+    def name(self):
+        return "action_search_course"
+
+    def run(self, dispatcher, tracker, domain):
+        if random() > 0.5:
+            dispatcher.utter_template("utter_course_list", tracker)
+        else:
+            sports_category = next(tracker.get_latest_entity_values("sports_category"), None)
+            cooking_category = next(tracker.get_latest_entity_values("cooking_category"), None)
+            category_entity = sports_category or cooking_category
+            if category_entity:
+                dispatcher.utter_template("utter_no_course_name", tracker, course_name=category_entity)
+            else:
+                dispatcher.utter_template("utter_no_course", tracker)
         return []
